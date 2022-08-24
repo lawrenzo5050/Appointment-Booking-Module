53fc9f4fcf706cf70986df3c6a1dd041d1902924
emmanuelmaiye <43306582+emmanuelmaiye@users.noreply.github.com>
2022-08-24 20:08:57 +0100

    sign-in-page

--- Flip-Fleep/Booking-Plugin/assets/js/formstyle.js
+++ Flip-Fleep/Booking-Plugin/assets/js/formstyle.js
@@ -103,3 +103,64 @@ function validateform(){
     return validate;
     
 }
+
+function CountryChange() {
+    var countryState = [
+        [
+            'NIGERIA', [
+            ['', 'State/Province'],
+            ['AL', 'Abuja'],
+            ['AK', 'Lagos'],
+            ['AZ', 'Ibadan'],
+            ['AR', 'Kaduna'],
+], ],
+[
+            'CA', [
+            ['', 'State/Province'],
+            ['AB', 'Alberta'],
+            ['BC', 'British Columbia'],
+            ['MB', 'Manitoba'],
+            ['NB', 'New Brunswick'],
+]]
+];
+
+    var countryElement = document.getElementById('countryId');
+    var stateElement = document.getElementById('stateId');
+    var stateLabelElement = document.getElementById('stateLabel');
+
+if (countryElement && stateElement) {
+        var listOfState = [
+            ['XX', 'None']
+        ];
+
+        var currentCountry = countryElement.options[countryElement.selectedIndex].value;
+        for (var i = 0; i < countryState.length; i++) {
+            if (currentCountry == countryState[i][0]) {
+                listOfState = countryState[i][1];
+            }
+        }
+        if (listOfstate.length < 2) 
+            {
+            stateElement.style.display = 'none';
+            stateLabelElement.style.display = 'none';
+            }
+    else 
+        {
+        stateElement.style.display = 'inline';
+        stateLabelElement.style.display = 'inline';
+        }
+        var selectedState;
+        for (var i = 0; i < stateElement.length; i++) {
+            if (stateElement.options[i].selected === true) {
+                selectedState = stateElement.options[i].value;
+            }     
+        }
+        stateElement.options.length = 0;
+        for (var i = 0; i < listOfState.length; i++) {
+            stateElement.options[i] = new Option(listOfState[i][1], listOfState[i][0]);
+            if (listOfState[i][0] == selectedState) {
+                stateElement.options[i].selected = true;
+            }    
+        }      
+    }
+}
